02b9afc52c5b7492dfee0f3fc47e939f632e2683
PriyanshGupta2002 <guptapriyansh23@gmail.com>
2023-10-02 12:04:20 +0530

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import { initalItems } from "./utils";
 
 const App = () => {
-  return <div className="text-3xl underline">App</div>;
+  const [count, setCount] = useState(0);
+  const [items] = useState(initalItems);
+  // const selectedItem = useMemo(() => items.find((i) => i.isSelected), [items]);
+  const selectedItem = useMemo(
+    () => items.find((i) => i.id === count),
+    [items, count]
+  );
+  return (
+    <div className="text-white flex items-center flex-col justify-center my-5 space-y-6">
+      <h2 className="text-3xl">Count : {count}</h2>
+      <p className="text-4xl">Selected Item : {selectedItem?.id}</p>
+      <button
+        className="bg-rose-700 p-2 rounded-md shadow-lg"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Count
+      </button>
+    </div>
+  );
 };
 
 export default App;
